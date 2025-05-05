39f922702d6703725cfc8910eac15c6856524d36
Sandipan Dey <hey@sandipan.dev>
2025-05-05 09:12:35 +0530

    chore: update website with blank bg just:

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,11 +1,6 @@
-import { HelloLoader } from "@/components/hello-loader"
-import { Picture } from "@/components/picture"
-
 const Home: React.FC = () => {
   return (
     <>
-      <HelloLoader />
-      <Picture />
     </>
   )
 }
